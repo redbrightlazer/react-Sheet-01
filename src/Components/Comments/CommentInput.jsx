d76dcc2aa473fe4b5142f86a92a0e1e1b0e432cd
redbrightlazer <fieryginz6@gmail.com>
2025-06-13 10:43:13 +0200

    nvm its not fixed ITS BROKENgit add .!

--- src/Components/Comments/CommentInput.jsx
+++ src/Components/Comments/CommentInput.jsx
@@ -1,27 +1,41 @@
 import { useState } from "react";
 import "./commentsC.css";
-import Comment from "./comments";
 import CommentList from "./commentList.jsx";
 
 function Input() {
   const [comments, setComments] = useState([]);
+
   function Submit(e) {
     e.preventDefault();
     let inp = e.target.elements[0].value;
-
     const newComment = {
       comment: inp,
-      author: "anonymous",
       date: "6-11-2025",
     };
 
     setComments([...comments, newComment]);
     e.target.reset();
   }
+
+  function aSub(e) {
+    e.preventDefault();
+    let anp = e.target.elements[0].value;
+    const newComment = {
+      author: anp,
+      date: "6-11-2025",
+    };
+
+    setComments([...comments, newComment]);
+    e.target.reset();
+  }
+
   return (
     <>
       <form onSubmit={Submit}>
         <input type="text" placeholder="Comment something..." />
+      </form>
+      <form onSubmit={aSub}>
+        <input type="text" placeholder="Who are you?..." />
       </form>
       <CommentList comments={comments} author="anonymous " />
     </>
